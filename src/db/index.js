a12bf50360d1bcc76ca6421313baed42877f66ac
Bhavesh Jat <bhveshjat2006@gmail.com>
2026-03-25 17:11:41 +0530

    asyncHandler promise version

--- src/db/index.js
+++ src/db/index.js
@@ -10,7 +10,7 @@ const connectDB= async ()=>{
     } catch (error) {
         console.log(`MONGODB connection Failed: ${error}`);
         process.exit(1);
-    }
+    } 
 }
 
 export default connectDB
