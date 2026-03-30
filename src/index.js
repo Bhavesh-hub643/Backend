47bf4901c76992f0004dd15796c38e568a393ae9
Bhavesh Jat <bhveshjat2006@gmail.com>
2026-03-30 19:51:36 +0530

    all setup configured.. user routes and controller basics added

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ dotenv.config({path: './env'});
 
 connectDB()
 .then(()=>{
-    app.listen(`${process.env.PORT}||5000`,()=>{
+    app.listen(process.env.PORT||5000,()=>{
         console.log(`app listening at port: ${process.env.PORT}`);
     })
 })
